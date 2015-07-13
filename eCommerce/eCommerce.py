aeb1af25cef07d76a1a4d15928bdc9299a534679
Maxime Paradis <max@faeriol.me>
2015-07-13 19:27:36 -0400

    Use template inheritence
    Provide Dummy Data fixture
    Provide Dummy Search

--- eCommerce/eCommerce.py
+++ eCommerce/eCommerce.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import Form, RecaptchaField
 from flask_wtf.file import FileField
@@ -9,26 +9,57 @@ from flask import request
 from wtforms import TextField, HiddenField, ValidationError, RadioField,\
     BooleanField, SubmitField, IntegerField, FormField, validators
 from wtforms.validators import Required
+import csv
+
+ISBN = 4
+CATEGORY = 2
+CATEGORIES = ["Science", "Technology", "Fantasy"]
 
 app = Flask(__name__)
-Bootstrap(app)
 
-@app.route('/book/<id>') # Where ID's are actually ISBN? 
+def getData():
+    result = []
+    with open('data.csv') as data:
+        reader = csv.reader(data, delimiter='|', quotechar='"', quoting=csv.QUOTE_ALL)
+        for row in reader:
+            result += [row]
+    return result
+
+@app.route('/book/<id>') # Where ID's are actually ISBN
 def book(id):
-    pass # Would crashes anyways bros
-    # Mock
-    # Fetch bookinfo
-    return render_template('book.html', form=book)
+    result = []
+    for book in getData():
+        if id == book[ISBN]:
+            result = book
+            break
+    return render_template('book.html', form=result)
 
 @app.route('/search/', methods=["GET"])
 def search():
     print(request)
     term = request.args.get('term')
-    ## Here mock behavior, probably render some prepared template
-    return "You searched for " +term
+    books = []
+    if term == None:
+        flash('Please use the search field to find content.', 'error')
+        return render_template("search.html", result={"header":[],"data":[]})
+    if term == "":
+        flash('The search term was empty. Please provide a valid search term', 'error')
+        return render_template("search.html", result={"header":[],"data":[]})
+    for book in getData():
+        for field in book:
+            if term in field:
+                print("FOUND")
+                books += [book]
+                break
+    return render_template("search.html", result={"header":["Title", "Author", "Category", "Description", "ISBN" ],"data":books})
 
 @app.route('/category/<cat>')
 def category(cat):
+    result = []
+    for book in getData():
+        if id == book[ISBN]:
+            result = book
+            break
     # we has got partial feature... Put in some books
     return render_template("category.html")
 
@@ -38,5 +69,7 @@ def index():
     return render_template("index.html")
 
 if __name__=="__main__":
+    Bootstrap(app)
     app.debug = True # Verbose browser output. Evil in prod.
+    app.config['SECRET_KEY'] = 'devkey'
     app.run()
