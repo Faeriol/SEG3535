913140f84b93b6dc01b0e0420996f17c06e92afa
Amir Gaouaou <amir.gaouaou@gmail.com>
2015-07-19 03:40:57 -0400

    Drawer working (not completely)

---
+++ PillManager/app/src/main/java/ca/uottawa/site/seg23525/projet/drmario/UI/HomeFragment.java
@@ -0,0 +1,26 @@
+package ca.uottawa.site.seg23525.projet.drmario.UI;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ca.uottawa.site.seg23525.projet.drmario.R;
+
+
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return rootView;
+    }
+}
+
