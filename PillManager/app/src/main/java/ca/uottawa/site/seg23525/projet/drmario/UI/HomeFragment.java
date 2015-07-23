66a72d14ee4be7946d0a6c7d8dfee9595f17c2f2
Maxime Paradis <max@faeriol.me>
2015-07-22 23:15:27 -0400

    Start of dynamically generated Home

--- PillManager/app/src/main/java/ca/uottawa/site/seg23525/projet/drmario/UI/HomeFragment.java
+++ PillManager/app/src/main/java/ca/uottawa/site/seg23525/projet/drmario/UI/HomeFragment.java
@@ -1,9 +1,13 @@
 package ca.uottawa.site.seg23525.projet.drmario.UI;
 
+    import android.content.Context;
     import android.os.Bundle;
+    import android.support.v7.widget.CardView;
     import android.view.LayoutInflater;
     import android.view.View;
     import android.view.ViewGroup;
+    import android.widget.Button;
+    import android.widget.LinearLayout;
 
     import java.util.List;
 
@@ -14,6 +18,7 @@ package ca.uottawa.site.seg23525.projet.drmario.UI;
 
 public class HomeFragment extends DAOFragment {
 
+    private LinearLayout layout;
 
     public HomeFragment(){}
 
@@ -21,15 +26,34 @@ public class HomeFragment extends DAOFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        checkDAO();
+
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        layout = (LinearLayout) rootView;
 
         List<PrescribedMedication> pMed = this.dao.getAllPrescribedMedication();
         for(PrescribedMedication m : pMed){
-            System.out.println(pMed);
+            createCard(m);
         }
 
         return rootView;
     }
 
+
+    private void createCard(PrescribedMedication m){
+        Context context = getActivity().getApplicationContext();
+        CardView cardView = new CardView(context);
+        cardView.setCardElevation(5);
+
+        ViewGroup.MarginLayoutParams params = new ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.setMargins(16, 16, 16, 16);
+
+        cardView.setLayoutParams(new LinearLayout.LayoutParams(params));
+        layout.addView(cardView);
+
+        Button cancelButton = new Button(context);
+        cancelButton.setText("Cancel");
+        cardView.addView(cancelButton);
+    }
 }
 
