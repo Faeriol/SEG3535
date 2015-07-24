e1d0a1b2437199b0c0842e82f4449202f9a7cd02
Maxime Paradis <max@faeriol.me>
2015-07-23 23:25:15 -0400

    WOOO It's pretty now

--- PillManager/app/src/main/java/ca/uottawa/site/seg23525/projet/drmario/UI/views/PrescribedMedCard.java
+++ PillManager/app/src/main/java/ca/uottawa/site/seg23525/projet/drmario/UI/views/PrescribedMedCard.java
@@ -1,6 +1,7 @@
 package ca.uottawa.site.seg23525.projet.drmario.UI.views;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.support.v7.widget.CardView;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -9,6 +10,10 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.Calendar;
+import java.util.Formatter;
+
+import ca.uottawa.site.seg23525.projet.drmario.R;
 import ca.uottawa.site.seg23525.projet.drmario.data.model.Medical.PrescribedMedication;
 
 /**
@@ -22,14 +27,16 @@ public class PrescribedMedCard extends CardView{
     private RelativeLayout rlayout;
     private int elevation = 5;
     private int bm = 16; // Base Margin
+    private Calendar c;
 
     //Components
     private ImageView imageView;
-    private TextView medName, medCommonName;
+    private TextView medName, medCommonName, dosage, dueTime;
     private Button cancelButton, postponeButton;
 
     public PrescribedMedCard(Context context, PrescribedMedication m) {
         super(context);
+        c = Calendar.getInstance();
         this.m = m;
         setCardElevation(elevation);
         this.id = baseId;
@@ -46,34 +53,72 @@ public class PrescribedMedCard extends CardView{
         addView(rlayout);
 
 
-
+        this.createImage();
         this.createTexts();
         this.createButtons();
 
+    }
+
+    private void createImage() {
+        imageView = new ImageView(this.getContext());
+        imageView.setId(id++);
+        imageView.setImageResource(R.mipmap.ic_launcher);
+        RelativeLayout.LayoutParams lay = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        lay.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        lay.addRule(RelativeLayout.ALIGN_PARENT_START);
+        lay.setMargins(bm, bm, bm, bm);
+        imageView.setLayoutParams(lay);
+        rlayout.addView(imageView);
     }
 
     private void createTexts() {
         medName  = new TextView(this.getContext());
         medName.setId(id++);
         medName.setText(m.getMedication().getName());
+        medName.setTypeface(null, Typeface.BOLD);
         RelativeLayout.LayoutParams lay = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         lay.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-        lay.addRule(RelativeLayout.ALIGN_PARENT_START);
+        lay.addRule(RelativeLayout.RIGHT_OF, imageView.getId());
         lay.setMargins(bm, bm, bm, bm);
         medName.setLayoutParams(lay);
 
         rlayout.addView(medName);
+
+        dosage = new TextView(this.getContext());
+        dosage.setId(id++);
+        dosage.setText(m.getDosage() + "mg");
+        lay = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        lay.addRule(RelativeLayout.RIGHT_OF, medName.getId());
+        lay.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        lay.setMargins(bm, bm, bm, bm);
+        dosage.setLayoutParams(lay);
+
+        rlayout.addView(dosage);
 
         medCommonName  = new TextView(this.getContext());
         medCommonName.setId(id++);
         medCommonName.setText("\"" + m.getMedication().getCommonName() + "\"");
         lay = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         lay.addRule(RelativeLayout.BELOW, medName.getId());
-        lay.addRule(RelativeLayout.ALIGN_PARENT_START);
+        lay.addRule(RelativeLayout.RIGHT_OF, imageView.getId());
         lay.setMargins(bm, bm, bm, bm);
         medCommonName.setLayoutParams(lay);
 
         rlayout.addView(medCommonName);
+
+        dueTime  = new TextView(this.getContext());
+        dueTime.setId(id++);
+        c.add(Calendar.HOUR_OF_DAY, 2);
+        Formatter f = new Formatter();
+        f.format("%tl:%tM", c, c);
+        dueTime.setText("Take at: " +f.toString());
+        lay = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        lay.addRule(RelativeLayout.BELOW, medCommonName.getId());
+        lay.addRule(RelativeLayout.RIGHT_OF, imageView.getId());
+        lay.setMargins(bm, bm, bm, bm);
+        dueTime.setLayoutParams(lay);
+
+        rlayout.addView(dueTime);
     }
 
     private void createButtons(){
